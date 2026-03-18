401be550103c5932e8a512d7b310556cb5e66ba1
Alok Kumar <alokbuilds.dev@gmail.com>
2026-03-18 06:05:17 +0530

    animations added

---
+++ script.js
@@ -0,0 +1,14 @@
+/* 🔥 Scroll Animation */
+const sections = document.querySelectorAll('.section');
+
+window.addEventListener('scroll', () => {
+    const trigger = window.innerHeight * 0.8;
+
+    sections.forEach(section => {
+        const top = section.getBoundingClientRect().top;
+
+        if (top < trigger) {
+            section.classList.add('show');
+        }
+    });
+});
